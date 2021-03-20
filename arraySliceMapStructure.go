2d1937cd0f46baca0c71a5b5fea9ae6300774a55
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-03-21 00:07:41 +0600

    Array

--- arraySliceMapStructure.go
+++ arraySliceMapStructure.go
@@ -1,13 +1,25 @@
 package main
 
 import "fmt"
-
+import "reflect"
 func main(){
-	//Array
-	var arrName [3]string 
-	arrName[0] = "Rafi"
-	arrName[1] = "shaon"
-	arrName[2]= "samim"
+	newArr := [3]int{1,2,3}
+
+	//Slice declaration
+	slice1 := newArr[0 : 2]
+	slice2 := append(slice1, 4, 5)
+	slice3 := make([]int,4)
+
+	//Copying the element of slice2 to slice3 using built in function
+	 copy(slice3, slice2)
 	
-	fmt.Println(arrName)
+	//Getting the underlying type of variable type and printing on screen
+	t := reflect.TypeOf(slice1).Kind().String() 
+	fmt.Println(t)
+	
+	//printing the type of slice1 variable data type
+	fmt.Printf("%T\n", slice1) 
+
+	//Printing all the data of the created slice 
+	fmt.Println(slice1, slice2, slice3) 
 }
