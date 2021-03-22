c6ba401e66c743ac88bfd8bddb07a8b29890c456
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-03-22 14:08:03 +0600

    maps

---
+++ maps.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    //var menu map[string]float64
+    //menu = map[string]float64{
+    	//"eggs": 1.75,
+    	//"bacon": 3.22,
+    	//"sausage": 1.89,
+	//}
+	
+	//fmt.Println(menu["eggs"])
+	//var x map[string]int
+	 x := map[string]int{
+	
+	"key" : 10,
+	"name" : 11,
+	"ki" : 32,
+	}
+	
+	//x["key"] = 10
+	fmt.Println(x["key"])
+	
+
+
+}
