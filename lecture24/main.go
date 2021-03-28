00930f198cf2b38e2b53d40f0e468f7d68621e80
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-03-28 15:32:11 +0600

    File creating Function

--- lecture24/main.go
+++ lecture24/main.go
@@ -25,21 +25,25 @@ func main() {
 		fmt.Println(err.Error())
 	}
 	fmt.Println(n, err)
-	createFile("another.txt")
 
+	//Calling file creating function
+	content := "fileName and contet are taking from the argument"
+	isFalse := createFile("another2.txt", content)
+	fmt.Println(isFalse)
 }
 
 //Creating a function to create file
-func createFile(fileName string) {
+func createFile(fileName, fileContent string) bool {
 	posf, err := os.Create(fileName)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer posf.Close()
 
-	n, err := posf.Write([]byte("Creating a file from the function"))
+	_, err = posf.Write([]byte(fileContent))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(n, err)
+	//fmt.Println(n, err)
+	return true
 }
