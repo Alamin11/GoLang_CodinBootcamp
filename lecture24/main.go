27cb83a0c27efc87d33640c3c5a2612b0a0ab129
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-03-29 03:54:43 +0600

    How to create a folder[C

--- lecture24/main.go
+++ lecture24/main.go
@@ -7,43 +7,50 @@ import (
 
 func main() {
 	//Showing directory path of working diectory
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(dir)
+	// dir, err := os.Getwd()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(dir)
 
 	//Creating directory
-	posf, err := os.Create("newFile.txt")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer posf.Close()
+	// posf, err := os.Create("newFile.txt")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// defer posf.Close()
 
-	n, err := posf.Write([]byte("This a file that is created using go lang"))
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(n, err)
+	// n, err := posf.Write([]byte("This a file that is created using go lang"))
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(n, err)
 
 	//Calling file creating function
-	content := "fileName and contet are taking from the argument"
-	isFalse := createFile("another2.txt", content)
-	fmt.Println(isFalse)
+	// content := "fileName and contet are taking from the argument"
+	// isFalse := createFile("another2.txt", content)
+	// fmt.Println(isFalse)
 
 	//checking if file is existing or not
-	fi, err := os.Stat("another.txt")
+	// fi, err := os.Stat("another.txt")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(fi.ModTime().Clock())
+	// fmt.Println(fi.ModTime().Date())
+	// fmt.Println(fi.ModTime().GobEncode())
+	// fmt.Println(fi.IsDir())
+	// fmt.Println(fi.ModTime())
+	// fmt.Println(fi.Name())
+	// fmt.Println(fi.Size())
+	// fmt.Println(fi.Sys())
+
+	//Creating a folder
+	err := os.Mkdir("lec25", 0777)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(fi.ModTime().Clock())
-	fmt.Println(fi.ModTime().Date())
-	fmt.Println(fi.ModTime().GobEncode())
-	fmt.Println(fi.IsDir())
-	fmt.Println(fi.ModTime())
-	fmt.Println(fi.Name())
-	fmt.Println(fi.Size())
-	fmt.Println(fi.Sys())
+	//fmt.Println(dirName)
 }
 
 //Creating a function to create file
