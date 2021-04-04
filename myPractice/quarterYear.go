5c8d8864ca5a67fffd405e997135d5be847a087d
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-04 12:24:30 +0600

    quarter Year

---
+++ myPractice/quarterYear.go
@@ -0,0 +1,17 @@
+package main
+
+func mul(month int) int {
+	x := float32(month)
+	if x/4.0 > 0.0 && x/4.0 <= 1.0 {
+		return 1
+	} else if x/4.0 > 1 && x/4.0 <= 2.0 {
+		return 2
+	} else {
+		return 3
+	}
+}
+
+// func main() {
+// 	res := mul(11)
+// 	fmt.Println(res)
+// }
