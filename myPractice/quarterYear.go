7cc731566d3e4bca3dadbe729a5a22bd634c78c6
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-06 04:24:21 +0600

    newUpdate

--- myPractice/quarterYear.go
+++ myPractice/quarterYear.go
@@ -3,17 +3,21 @@ package main
 import "fmt"
 
 func mul(month int) int {
-	x := float32(month)
-	if x/4.0 > 0.0 && x/4.0 <= 1.0 {
-		return 1
-	} else if x/4.0 > 1 && x/4.0 <= 2.0 {
-		return 2
-	} else {
-		return 3
+	if month != 0 {
+		x := float32(month)
+		if x/4.0 > 0.0 && x/4.0 <= 1.0 {
+			return 1
+		} else if x/4.0 > 1 && x/4.0 <= 2.0 {
+			return 2
+		} else {
+			return 3
+		}
+
 	}
+	return 0
 }
 
 func main() {
-	res := mul(11)
+	res := mul(2)
 	fmt.Println(res)
 }
