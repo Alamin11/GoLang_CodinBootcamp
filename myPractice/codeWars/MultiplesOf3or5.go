950041ec07d704f44c2271094b99e02803c271a8
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-07 09:12:02 +0600

    Multipl of 3 or 5

--- myPractice/codeWars/MultiplesOf3or5.go
+++ myPractice/codeWars/MultiplesOf3or5.go
@@ -1,14 +1,19 @@
 package main
 
+import "fmt"
+
 func Multiple3And5(number int) int {
 	sum := 0
-	for i := 3; i <= number; i++ {
+	for i := 3; i < number; i++ {
 		if i%3 == 0 || i%5 == 0 {
-			sum = sum + i
+			if (i%3 == 0 && i%5 != 0) || (i%3 != 0 && i%5 == 0) {
+				sum += i
+			}
 		}
 	}
 	return sum
 }
 func main() {
-
+	x := Multiple3And5(63)
+	fmt.Println(x)
 }
