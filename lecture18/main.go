cffef4cc46cc3607af0f29e00979605c0f617718
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-13 02:56:18 +0600

    golangTemplate

--- lecture18/main.go
+++ lecture18/main.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 	http.HandleFunc("/", home)
-	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8889", nil)
 }
 func home(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `Hello , I'm ok`)
+}
+func features(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
+	ptmp, err = ptmp.ParseFiles("wPage/features.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `Hello , I'm ok`)
+}
+
+func docs(w http.ResponseWriter, r *http.Request) {
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wPage/docs.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
 	//fmt.Fprintf(w, `Hello , I'm ok`)
 }
