523c5296e89f56b1ef8d34a173d0fbb6a9c4f1ad
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-21 22:25:11 +0600

    For Loop

--- forLoop/for.go
+++ forLoop/for.go
@@ -10,5 +10,22 @@ func main() {
 		fmt.Printf("%d ", i)
 		i++
 	}
+	fmt.Printf("\n")
+	// for loop with condition
+	for j := 1; j <= 10; j++ {
+		fmt.Printf("%d ", j)
+	}
+	fmt.Printf("\n")
+	//for loop without condition
+	for {
+		fmt.Println("Infinite Loop")
+		break
+	}
+	for n := 0; n <= 10; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 
 }
