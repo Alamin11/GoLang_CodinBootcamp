4f3469fa5e072621967bfa2ec08c44c4753db88d
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-21 22:11:15 +0600

    backLog

---
+++ forLoop/for.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//while like loop
+	i := 1
+	for i <= 10 {
+		fmt.Printf("%d ", i)
+		i++
+	}
+
+}
---
+++ myPractice/error/fmtMath.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	r := math.Sqrt(25)
+	fmt.Println(r)
+}
