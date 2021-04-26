b229605b02d7fb69749b683a3aaa024b8524ab64
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-04-26 18:29:47 +0600

    loop

--- forLoop/for.go
+++ forLoop/for.go
@@ -28,4 +28,26 @@ func main() {
 		fmt.Println(n)
 	}
 
+	//Range for loop
+	// Example 1
+	strDict := map[string]string{"Bangladesh": "Dhaka", "Japan": "Tokyo", "China": "Beijing", "Canada": "Ottawa"}
+	for index, element := range strDict {
+		fmt.Println("Index :", index, " Element :", element)
+	}
+
+	// Example 2
+	for key := range strDict {
+		fmt.Println(key)
+	}
+
+	// Example 3
+	for _, value := range strDict {
+		fmt.Println(value)
+	}
+
+	//range loop over string
+	for range "Hello" {
+		fmt.Println("Hello")
+	}
+
 }
