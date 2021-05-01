cb32409085a28ac9d3b404c56044eb6a19346418
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-05-02 04:09:11 +0600

    lec16 updated

--- classLecture/lecture16/http/main.go
+++ classLecture/lecture16/http/main.go
@@ -14,30 +14,33 @@ func main() {
 		os.Exit(1)
 	}
 	//Accepting the request
-	conn, err := nl.Accept()
-	if err != nil {
-		fmt.Println(err.Error())
+	for {
+		conn, err := nl.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		bs := make([]byte, 1024) //a byte slice to store the request
+		//reading the request as byte string
+		n, err := conn.Read(bs) //kotha shona
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println(n)
+
+		//converting byte string bs to normal string
+		reqStr := string(bs) //conversion (between two diffrent types)
+		fmt.Println(reqStr)
+
+		body := `<!DOCTYPE html><html lang ="en"><head><title>networking</title></head><body><h1>This is a testing string <br> <p>New string added</p></h1></body></html>`
+
+		resp := "HTTP/1.1 200 OK\r\n" +
+			"Content-Length: %d\r\n" +
+			"Content-Type: text/html\r\n" +
+			"\r\n%s"
+
+		msg := fmt.Sprintf(resp, len(body), body)
+		fmt.Println(msg)        //displaying response message
+		conn.Write([]byte(msg)) //kotha bola
+
 	}
-	bs := make([]byte, 1024)
-	//reading the request as byte string 
-	n, err := conn.Read(bs)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(n)
-
-	//converting byte string bs to normal string
-	reqStr := string(bs)
-	fmt.Println(reqStr)
-
-	body := `<!DOCTYPE html><html lang ="en"><head><title>networking</title></head><body><h1>This is a testing string <br> <p>New string added</p></h1></body></html>`
-
-	resp := "HTTP/1.1 200 OK\r\n" +
-		"Content-Length: %d\r\n" +
-		"Content-Type: text/html\r\n" +
-		"\r\n%s"
-
-	msg := fmt.Sprintf(resp, len(body), body)
-	fmt.Println(msg)
-	conn.Write([]byte(msg))
 }
