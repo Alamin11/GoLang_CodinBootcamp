001bc2aee5e1495daf5e9da91a36e3330093e0fa
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-05-03 14:30:44 +0600

    Static file handling

--- myPractice/httpServer/main.go
+++ myPractice/httpServer/main.go
@@ -8,7 +8,14 @@ import (
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
+	http.HandleFunc("/staticFile", content)
+	//http.HandleFunc("/contact", contact)
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/resourse/", http.StripPrefix("/resourse/", fs))
 
+	//http.Handle("/res/", http.StripPrefix("/res/", http.FileServer(http.Dir("assets"))))
+	//fs := http.FileServer(http.Dir("assets/"))
+	//http.Handle("/res/", http.StripPrefix("/res/", fs))
 	http.ListenAndServe(":8888", nil)
 
 }
@@ -19,3 +26,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `Welcome to my website , Happy exploring about me `)
 }
+
+func content(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	fmt.Fprintf(w, "<img src=\"venom.jpg\">")
+
+}
+
+// func contact(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+// 	fmt.Fprintf(w, "<img src=\"res/venom.jpg\"/>")
+// }
