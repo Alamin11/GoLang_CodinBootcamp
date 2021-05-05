c53bdd9906cd4d64c4a3fcc029bd7bdff03ae411
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-05-06 00:24:11 +0600

    form handling method 1

--- myPractice/templatingWebsite/main.go
+++ myPractice/templatingWebsite/main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/docs", docs)
 	http.HandleFunc("/features", features)
+	http.HandleFunc("/request", request)
 	fs := http.FileServer(http.Dir("./assets"))
 	http.Handle("/res/", http.StripPrefix("/res/", fs))
 
@@ -51,32 +52,21 @@ func features(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, `Welcome here in the loop of limitless`)
 }
 
-// func docs(w http.ResponseWriter, r *http.Request) {
-// 	ptmp, err := template.ParseFiles("template/base.gohtml")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	//ptmp.Execute(w, nil)
-// 	ptmp, err = template.ParseFiles("wpage/docs.gohtml")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	ptmp.Execute(w, nil)
-// 	//fmt.Fprintf(w, `Welcome here in the loop of limitless`)
-// }
+func request(w http.ResponseWriter, r *http.Request) {
+	//form value handling
+	//method 1
+	// name := r.FormValue("name")
+	// company := r.FormValue("company")
+	// email := r.FormValue("email")
 
-// func features(w http.ResponseWriter, r *http.Request) {
-// 	ptmp, err := template.ParseFiles("template/base.gohtml")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
-// 	//ptmp.Execute(w, nil)
+	// fmt.Println(name, company, email)
 
-// 	ptmp, err = template.ParseFiles("wpage/docs.gohtml")
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 	}
+	r.ParseForm()
+	for key, val := range r.Form {
+		fmt.Println(key, val)
+	}
 
-// 	ptmp.Execute(w, nil)
-// 	//fmt.Fprintf(w, `Welcome here in the loop of limitless`)
-// }
+	fmt.Fprintf(w, `recieved`)
+	//ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `Welcome here in the loop of limitless`)
+}
