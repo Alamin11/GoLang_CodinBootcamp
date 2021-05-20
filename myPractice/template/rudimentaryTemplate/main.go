346fd5d417bc9e1fb7961ab3c6cb8b9bd605b6bb
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-05-20 22:43:19 +0600

    templating using concatenation and file creating using os package

--- myPractice/template/rudimentaryTemplate/main.go
+++ myPractice/template/rudimentaryTemplate/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	name := "Mohammad Al amin"
+	//name := "Mohammad Al amin"
+	name := os.Args[1] //storing the data taken from user screen
+	fmt.Println(os.Args[0])
+	fmt.Println(os.Args[1])
 	tmp := `
 	<!DOCTYPE html>
 	<html lang="eng">
@@ -15,5 +23,12 @@ func main() {
 		</body>
 	</html>
 	`
+	//creating a file and taking data from user using os.Create() package
+	nf, err := os.Create("index.html")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer nf.Close()
+	io.Copy(nf, strings.NewReader(tmp))
 	fmt.Println(tmp)
 }
