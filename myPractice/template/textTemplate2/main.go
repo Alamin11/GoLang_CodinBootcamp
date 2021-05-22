71f6dca5e4c33c51255c8a281a4c3de21a72bc59
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-05-23 00:19:35 +0600

    Parsing files using ParseGlobe from template func Must to handle error and executing them

--- myPractice/template/textTemplate2/main.go
+++ myPractice/template/textTemplate2/main.go
@@ -6,14 +6,33 @@ import (
 	"text/template"
 )
 
-func main() {
-	tpl, err := template.ParseFiles("tpl.gohtml")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	err = tpl.Execute(os.Stdout, nil)
+var tpl *template.Template
 
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+func main() {
+	// tpl, err := template.ParseFiles("tpl.gohtml")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// err = tpl.Execute(os.Stdout, nil)
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	//Using execute func from parseGlob
+	err := tpl.ExecuteTemplate(os.Stdout, "template1.gohtml", nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	err = tpl.ExecuteTemplate(os.Stdout, "template2.gohtml", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "template3.gohtml", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
